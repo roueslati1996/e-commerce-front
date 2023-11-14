9fd6b424823dbbd6000ec6a88745641e1343438a
rihab.oueslati <rihaboueslati@takiacademyteam.com>
2023-11-14 16:16:21 +0100

    add pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,24 @@ import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './layout/header/header.component';
+import { ShopComponent } from './pages/shop/shop.component';
+import { ProductComponent } from './pages/product/product.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { AddressComponent } from './pages/address/address.component';
+import { ShippingComponent } from './pages/shipping/shipping.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, RegisterComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HeaderComponent,
+    ShopComponent,
+    ProductComponent,
+    CartComponent,
+    AddressComponent,
+    ShippingComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
