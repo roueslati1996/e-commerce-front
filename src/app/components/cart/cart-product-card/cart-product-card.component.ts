52783322d613ada967570efbf0c6d36543acc532
rihab.oueslati <rihaboueslati@takiacademyteam.com>
2023-11-17 17:10:19 +0100

    add some logic to cart

--- src/app/components/cart/cart-product-card/cart-product-card.component.ts
+++ src/app/components/cart/cart-product-card/cart-product-card.component.ts
@@ -1,10 +1,27 @@
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cart-product-card',
   templateUrl: './cart-product-card.component.html',
-  styleUrls: ['./cart-product-card.component.scss']
+  styleUrls: ['./cart-product-card.component.scss'],
 })
 export class CartProductCardComponent {
+  quantity: number = 2;
 
+  ngOnInit() {
+    console.log(this.quantity);
+  }
+
+  upgradeQuantity(): void {
+    this.quantity = this.quantity + 1;
+  }
+
+  downgradeQuantity(): void {
+    if (this.quantity > 0) this.quantity = this.quantity - 1;
+  }
+
+  removeQuantity(): void {
+    this.quantity = 0;
+  }
 }
