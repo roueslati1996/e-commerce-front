7d88815ea071e1e9b88f41079953eba8c0323e09
mohamed-tarek-guezguez <tarekgzgz@gmail.com>
2023-11-21 08:22:41 +0100

    Added register page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LoginComponent } from './components/auth/login/login.component';
 import { ShopComponent } from './pages/shop/shop.component';
 import { ProductComponent } from './pages/product/product.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { AddressComponent } from './pages/address/address.component';
 import { ShippingComponent } from './pages/shipping/shipping.component';
-import { RegisterComponent } from './components/auth/register/register.component';
 
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
+
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./components/auth/routes.module').then((m) => m.Module),
+  },
 
   {
     path: 'products',
