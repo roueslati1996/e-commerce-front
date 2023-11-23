4db7ad6b314efc453e97963b39cf0e42a3571c81
rihab.oueslati <rihaboueslati@takiacademyteam.com>
2023-11-23 12:17:54 +0100

    fix build error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,8 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
-import { ProductComponent } from './components/products/view-product-details/product.component';
 import { CartComponent } from './components/cart/view-cart/cart.component';
 import { AddressComponent } from './components/cart/address/address.component';
-import { ShippingComponent } from './components/cart/shipping/shipping.component';
-import { ProductCardComponent } from './components/products/product-card/product-card.component';
 import { OrderSummaryComponent } from './components/cart/order-summary/order-summary.component';
 import { CartProductCardComponent } from './components/cart/cart-product-card/cart-product-card.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
@@ -20,11 +17,8 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ProductComponent,
     CartComponent,
     AddressComponent,
-    ShippingComponent,
-    ProductCardComponent,
     OrderSummaryComponent,
     CartProductCardComponent,
     NotFoundComponent,
